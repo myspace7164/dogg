9a49ea3e312c34289b2ecf54d379f14397d273b2
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-11-16 11:40:11 +0100

    add rudimentary player object

---
+++ spieler.lua
@@ -0,0 +1,11 @@
+Spieler = Object.extend(Object)
+Spielers = Object.extend(Object)
+
+function Spieler.new(self)
+	self.i_spieler = 0
+	self.murmeln = {}
+	self.karten = {}
+end
+
+function Spielers.new(self)
+end
